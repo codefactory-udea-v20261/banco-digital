23ecead14a2268eaf5ae1fd2756db2773c40b1a6
Manuel Felipe <manuel.alvarez2@udea.edu.co>
2026-04-02 00:08:01 -0500

    feat(accounts): add ConsultarSaldoResponseDto

--- src/main/java/com/udea/bancodigital/accounts/application/usecase/ConsultarSaldoUseCase.java
+++ src/main/java/com/udea/bancodigital/accounts/application/usecase/ConsultarSaldoUseCase.java
@@ -1,7 +1,29 @@
 package  com.udea.bancodigital.accounts.application.usecase;
+import java.util.UUID;
+
+import com.udea.bancodigital.accounts.application.dto.ConsultarSaldoResponseDto;
+import com.udea.bancodigital.accounts.domain.model.Cuenta;
+import com.udea.bancodigital.accounts.domain.port.out.CuentaRepositoryPort;
+
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 public class ConsultarSaldoUseCase {
+    /*se trae el repositorio  de cuenta para buscar la id de la cuenta*/
+    private final CuentaRepositoryPort cuentaRepository;
+
+    /*se busca la cuenta en base a la id, en caso de no encontrarla
+    arroja un RuntimeException*/
+    public ConsultarSaldoResponseDto consulta(UUID cuentaId){
+        Cuenta cuenta=cuentaRepository.findById(cuentaId).orElseThrow(() -> new RuntimeException("Cuenta no encontrada"));
     
+    /*verifica que la cuenta este activa, en caso de no estarlo
+    arroja un RuntimeException*/
+    if (!cuenta.isActiva()){
+        throw new RuntimeException("La cuenta está inactiva");
+    }
+    return ConsultarSaldoResponseDto.builder()
+            .saldo(cuenta.getSaldo())
+            .build();    }
+
 }
